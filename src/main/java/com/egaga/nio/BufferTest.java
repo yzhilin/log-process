8424d775c095647f4ed855b4a8313329f2e5de90
yangzhilin <1074083778@qq.com>
2018-06-11 19:05:21 +0800

    log

--- src/main/java/com/egaga/nio/BufferTest.java
+++ src/main/java/com/egaga/nio/BufferTest.java
@@ -9,12 +9,33 @@ import java.nio.CharBuffer;
  */
 public class BufferTest {
     public static void main(String[] args) {
-        CharBuffer allocate = CharBuffer.allocate(8);
-        System.out.println("capacity: "+allocate.capacity());
-        System.out.println("limit:"+allocate.limit());
-        System.out.println("position:"+allocate.position());
-        //;
+        CharBuffer buffer = CharBuffer.allocate(8);
+        System.out.println("capacity: "+buffer.capacity());
+        System.out.println("limit:"+buffer.limit());
+        System.out.println("position:"+buffer.position());
+        //放入元素
+        buffer.put('a');
+        buffer.put('b');
+        buffer.put('c');
+        System.out.println("加入三个元素后，position="+buffer.position());
+        //调用flip（）方法
+        buffer.flip();
+        System.out.println("flip方法后,positon"+buffer.position());
+        System.out.println("capacity: "+buffer.capacity());
+        System.out.println("limit:"+buffer.limit());
+        System.out.println("position:"+buffer.position());
 
+        //取出第一个元素
+        System.out.println("第一个元素（position=0）:"+buffer.get());
+        System.out.println("capacity: "+buffer.capacity());
+        System.out.println("limit:"+buffer.limit());
+        System.out.println("position:"+buffer.position());
 
+        //调用clear
+        buffer.clear();
+        System.out.println("执行clear后,limit="+buffer.limit());
+        System.out.println("capacity: "+buffer.capacity());
+        System.out.println("limit:"+buffer.limit());
+        System.out.println("position:"+buffer.position());
     }
 }
